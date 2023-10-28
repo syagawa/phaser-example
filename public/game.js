a3c00459e05bb08be74f8329a9ae5cfa90a069eb
syagawa <syagawapp@gmail.com>
2023-10-28 14:44:52 +0900

    Part8 add stars

--- public/game.js
+++ public/game.js
@@ -17,6 +17,7 @@ const config = {
 };
 
 let player;
+let stars;
 let platforms;
 let cursors;
 
@@ -66,7 +67,20 @@ function create (){
 
   cursors = this.input.keyboard.createCursorKeys();
 
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+
   this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+
+  this.physics.add.overlap(player, stars, collectStar, null, this);
 
 }
 
@@ -87,4 +101,8 @@ function update (){
   if (cursors.up.isDown && player.body.touching.down){
     player.setVelocityY(-330);
   }
+}
+
+function collectStar(player, star){
+  star.disableBody(true, true);
 }

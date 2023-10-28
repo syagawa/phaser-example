8d569bd733562c32ffd4205a66cf2dbf7455584e
syagawa <syagawapp@gmail.com>
2023-10-28 15:24:16 +0900

    Fix app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("./public"));
 app.use((req, res, next) => {
-  console.log(req.url);
+  console.log("req.url: ", req.url);
+  next();
 });
 
+app.use(express.static("./public"));
 app.listen(port, () => {
   console.log("app started!!");
-})
+});

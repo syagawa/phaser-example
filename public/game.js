8ac70f4c52bb48da315a10dd127d3c6c5c908a27
syagawa <syagawapp@gmail.com>
2023-10-28 14:57:38 +0900

    Part10 add bomb

--- public/game.js
+++ public/game.js
@@ -18,9 +18,11 @@ const config = {
 
 let player;
 let stars;
+let bombs;
 let platforms;
 let cursors;
 let score = 0;
+let gameOver = false;
 let scoreText;
 
 const game = new Phaser.Game(config);
@@ -79,17 +81,27 @@ function create (){
     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
   });
 
+  bombs = this.physics.add.group();
+
   scoreText = this.add.text(16, 16, "score: 0", { fontSize: "32px", fill: "#000"});
 
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
 
   this.physics.add.overlap(player, stars, collectStar, null, this);
+
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 
 }
 
 
 function update (){
+
+  if(gameOver){
+    return;
+  }
+
   if (cursors.left.isDown){
     player.setVelocityX(-160);
 
@@ -112,4 +124,27 @@ function collectStar(player, star){
 
   score += 10;
   scoreText.setText("Score: " + score);
+
+  if (stars.countActive(true) === 0){
+    stars.children.iterate(function(child){
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+    let bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
+
+
+}
+
+
+function hitBomb (player, bomb){
+  this.physics.pause();
+  player.setTint(0xff0000);
+  player.anims.play('turn');
+  gameOver = true;
 }
